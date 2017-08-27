9a32afe0f41ba3c9d7a7a55f95dfcc4b2da09baa
Никита Ковальчук <ponayobi@gmail.com>
2017-08-27 19:53:50 +0300

    feat(filter): connect home page to redux store

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+import { createStructuredSelector } from 'reselect';
+import { List } from 'immutable';
+import { makeSelectPony } from './selectors';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
@@ -7,3 +12,19 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     );
   }
 }
+
+HomePage.propTypes = {
+  pony: PropTypes.instanceOf(List),
+};
+
+const mapStateToProps = createStructuredSelector({
+  pony: makeSelectPony(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
