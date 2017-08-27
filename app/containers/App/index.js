33e1c5ebdf5f1adf53adf9a2c405c8e6f5567269
Никита Ковальчук <ponayobi@gmail.com>
2017-08-27 19:49:14 +0300

    feat(project): add helmet to App component

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { MuiThemeProvider } from 'material-ui';
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -10,6 +11,13 @@ export default class App extends React.PureComponent { // eslint-disable-line re
   render() {
     return (
       <div>
+        <Helmet
+          titleTemplate="%s - Pony Store"
+          defaultTitle="Pony Store"
+          meta={[
+            { name: 'description', content: 'Pony store!' },
+          ]}
+        />
         <MuiThemeProvider>
           {this.props.children}
         </MuiThemeProvider>
